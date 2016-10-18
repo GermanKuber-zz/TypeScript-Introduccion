c03a72109a86c36e29fa9fb16be9b04cc60c1db0
Mariano Villarreal Kübel <GermanKuber@Marianos-MacBook-Pro.local>
2016-10-17 23:05:42 -0300

    Se agregan .ts

--- Robot/1 - main.js
+++
@@ -1,8 +0,0 @@
-function Scan(param) {
-    param.status = "Escaneado";
-    var removeValue = ((param.valor * 10) / 100);
-    param.valor = param.Valor - removeValue;
-    console.log("Se escaneo : " + param.name + " El valor actual es : " + para.valor);
-    return param;
-}
-Scan("Mensaje");
