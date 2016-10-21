92b61c2ce35246b2446e9a7907129077f0d94ea6
Germán Küber <german.kuber@outlook.com>
2016-10-21 18:18:07 -0300

    Se agrega

--- live/js/main.js
+++ live/js/main.js
@@ -1,7 +1,8 @@
-function procesar(param){
+function procesar(param) {
     param.name = "Primer Nombre";
     var calc = param + param;
     console.log(calc);
     return calc;
 }
-procesar("Mensaje");
+var valor = procesar("Mensaje");
+//# sourceMappingURL=main.js.map
