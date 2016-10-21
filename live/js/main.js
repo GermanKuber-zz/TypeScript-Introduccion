a209c0d8f6ace80f266ebab11894cc2f4d91dd04
Germán Küber <german.kuber@outlook.com>
2016-10-21 18:19:05 -0300

    se agerga carpeta

--- live/js/main.js
+++
@@ -1,8 +0,0 @@
-function procesar(param) {
-    param.name = "Primer Nombre";
-    var calc = param + param;
-    console.log(calc);
-    return calc;
-}
-var valor = procesar("Mensaje");
-//# sourceMappingURL=main.js.map
