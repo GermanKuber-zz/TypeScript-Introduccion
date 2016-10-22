66a986b817bf618fd91c634b6d78ba4ca1ef8a1c
Germán Küber <german.kuber@outlook.com>
2016-10-22 00:48:07 -0300

    Se agregan Generics

--- 09 - Generics.ts
+++ 09 - Generics.ts
@@ -1,5 +1,14 @@
 class GenericMethods<T>{
-    getAll():Array<T>{
-        return new Array<T>();
+    getAll<T>(param : T):T{
+        return param;
     }
 }
+
+var generic = new GenericMethods();
+
+var str = generic.getAll<string>("Valor");
+
+var str2 = generic.getAll("Valor");
+
+
+var str3 = generic.getAll(new Array<string>());
